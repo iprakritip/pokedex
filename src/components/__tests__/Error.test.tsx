79f1136a1cad05a8683ce5918d192bc2edee5305
Prakriti Pokharel <iprakritip@gmail.com>
2023-06-28 09:11:39 +0545

    test errorpage

---
+++ src/components/__tests__/Error.test.tsx
@@ -0,0 +1,14 @@
+import { render, screen } from "@testing-library/react";
+import React from "react";
+
+import '@testing-library/jest-dom'
+import Error from '../../pages/Error'
+
+describe('ErrorPage', () => { 
+    const TestComponent=(<Error/>)
+    it('renders properly',()=>{
+        render(TestComponent)
+        expect(screen.getByText(/Sorry!!!! Something went wrong/)).toBeInTheDocument()
+    })
+    //passed
+ })
