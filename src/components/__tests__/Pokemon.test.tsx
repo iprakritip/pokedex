cf1bb1f448c85b93c23d835ae7377a4c8124c0c3
Prakriti Pokharel <iprakritip@gmail.com>
2023-06-28 09:01:06 +0545

    test search

---
+++ src/components/__tests__/Pokemon.test.tsx
@@ -0,0 +1,33 @@
+import {render, screen} from '@testing-library/react';
+import React from 'react';
+import '@testing-library/jest-dom';
+import Pokemon from '../Pokemon';
+import {BrowserRouter} from 'react-router-dom';
+
+describe('Pokemon', () => {
+  const mockedFn = jest.fn();
+  const types = [
+    {pokemon_v2_type: {id: 1, name: 'grass'}},
+    {pokemon_v2_type: {id: 2, name: 'poison'}},
+  ];
+
+  const TestComponent = (
+    <BrowserRouter>
+      <Pokemon
+        togglePokeInfo={mockedFn}
+        id={1}
+        name='bulbasaur'
+        types={types}
+        changeSelectedPokemonId={mockedFn}
+        clickedPokemonId={1}
+      />
+    </BrowserRouter>
+  );
+
+  it('renders properly', () => {
+    render(TestComponent);
+    expect(screen.getByText('BULBASAUR')).toBeInTheDocument();
+  });
+
+  // passed
+});
