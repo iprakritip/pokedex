701e96d62fdd99911b953d5ec9c96874afc45b04
Prakriti Pokharel <iprakritip@gmail.com>
2023-06-28 07:30:00 +0545

    test footer

---
+++ src/components/__tests__/Footer.test.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Footer from '../Footer';
+import {render, screen} from '@testing-library/react';
+import '@testing-library/jest-dom';
+
+describe('footer', () => {
+  const TestComponent = (<Footer/>);
+  test('Renders properly', () => {
+    render(TestComponent);
+    expect(screen.getByText('Built by')).toBeInTheDocument();
+  });
+});
+
+//passed
