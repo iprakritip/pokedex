728c049e0a3f799d23a60a93238886c426540380
Prakriti Pokharel <iprakritip@gmail.com>
2023-06-28 09:04:58 +0545

    test type

---
+++ src/components/__tests__/Type.test.tsx
@@ -0,0 +1,12 @@
+import {render, screen} from '@testing-library/react';
+import React from 'react';
+import Type from '../Type';
+
+describe('Type', () => {
+  const TestComponent = <Type typeName='grass' typeId={1} />;
+  it('renders properly', () => {
+    render(TestComponent);
+    expect(screen.getByText('Grass')).toBeInTheDocument();
+  });
+  //passed
+});
