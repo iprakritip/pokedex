125606d6cf2f27610c3b4acc78947b7174eac64f
Prakriti Pokharel <iprakritip@gmail.com>
2023-06-29 09:00:57 +0545

    try testing index.js and App and fail

---
+++ src/components/__tests__/index.test.tsx
@@ -0,0 +1,24 @@
+import {render, screen} from '@testing-library/react';
+import React from 'react';
+import '@testing-library/jest-dom';
+import App from '../../App';
+import {BrowserRouter} from 'react-router-dom';
+import {ApolloClient, ApolloProvider, InMemoryCache} from '@apollo/client';
+
+const mockClient = new ApolloClient({
+  uri: 'https://beta.pokeapi.co/graphql/v1beta/',
+  cache: new InMemoryCache(),
+});
+
+describe('App', () => {
+  it('renders properly', () => {
+    render(
+      <ApolloProvider client={mockClient}>
+        <App />
+      </ApolloProvider>
+    );
+    expect(screen.getByRole('textbox')).toBeInTheDocument();
+  });
+
+  //passed in case of errorpage
+});
