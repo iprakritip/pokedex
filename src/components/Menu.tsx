072f32484660b94ad0807389f3cb586a002b725d
Prakriti Pokharel <iprakritip@gmail.com>
2023-06-29 09:59:55 +0545

    fix menus zindex

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -9,8 +9,8 @@ interface MenuProps {
 const Menu = ({toggleMenu, closeMenu}: MenuProps) => {
   return (
     <div data-testid='menu'
-      className={`h-screen w-[30vw] bg-white z-20 fixed ${
-        toggleMenu ? 'top-0 left-0' : 'left-[-35%]'
+      className={`h-screen w-[30vw] bg-white z-30 fixed ${
+        toggleMenu ? 'top-0 left-0' : 'left-[-35%] top-0'
       } transition-position duration-500 ease-in-out flex flex-col gap-8 items-center`}
     >
       <p className='w-full bg-green-500 h-[4rem] text-white text-xl font-semibold flex justify-between items-center px-6'>
